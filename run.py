20f7517e09884ab5461f436419d35625938cb679
Dm-Rb <dm.rb.418http@gmail.com>
2025-10-05 19:43:27 +0300

    Добавлен планировщик

--- run.py
+++ run.py
@@ -1,11 +1,25 @@
-from parsers.edostavka_by.controller import main
+from parsers.edostavka_by.controller import main as edostavka_by_run
+from parsers.gippo_market_by.controller import main as gippo_market_by_run
+
+import schedule
+import time
 
 
-            # manufacturers
-
+def start_scrapping():
+    try:
+        edostavka_by_run()
+    except Exception as ex:
+        print(ex)
+    #
+    try:
+        gippo_market_by_run()
+    except Exception as ex:
+        print(ex)
 
 
 if __name__ == "__main__":
-    main()
-    # factory = get_session_factory('catalog')
-    # service = CategoryService(factory)
+    schedule.every().day.at("01:30").do(start_scrapping)
+    while True:
+        schedule.run_pending()
+        time.sleep(60)
+
